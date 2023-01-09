ed53afd2c28de622bcb4f9264ccd82bd08bb4d33
fakynno <fakynno@gmail.com>
2023-01-09 18:24:47 -0300

    Add outro metodo p/ iterar sobre arrays

--- src/test/java/com/algaworks/junit/utilidade/FiltroNumerosTest.java
+++ src/test/java/com/algaworks/junit/utilidade/FiltroNumerosTest.java
@@ -1,11 +1,12 @@
 package com.algaworks.junit.utilidade;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 class FiltroNumerosTest {
 
     @Test
@@ -14,7 +15,17 @@ class FiltroNumerosTest {
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6);
         List<Integer> numerosParesEsperados = Arrays.asList(2, 4, 6);
         List<Integer> resultadoFiltro = FiltroNumeros.numerosPares(numeros);
-        Assertions.assertIterableEquals(numerosParesEsperados, resultadoFiltro);
+        assertIterableEquals(numerosParesEsperados, resultadoFiltro);
+    }
+
+    @Test
+    public void shouldReturnEvenNumbers2() {
+
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6);
+        List<Integer> numerosParesEsperados = Arrays.asList(2, 4, 6);
+        List<Integer> resultadoFiltro = FiltroNumeros.numerosPares(numeros);
+        assertArrayEquals(numerosParesEsperados.toArray(new Object[]{}),
+                resultadoFiltro.toArray(new Object[]{}));
     }
 
 }
