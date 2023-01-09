2e5deb586f98a4eb4a69c13e48c0aa4e40bd5d30
fakynno <fakynno@gmail.com>
2023-01-09 17:30:02 -0300

    Alteracao no metodo assert

--- src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
+++ src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
@@ -1,13 +1,14 @@
 package com.algaworks.junit.utilidade;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SaudacaoUtilTest {
 
     @Test
     public void saudar() {
         String saudacao = SaudacaoUtil.saudar(9);
-        Assertions.assertEquals("Bom dia", saudacao);
+        assertEquals("Bom dia", saudacao);
     }
 }
