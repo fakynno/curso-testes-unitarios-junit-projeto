30fe8f8ae9ffecc96cca4fc7cec1783ead6091d5
fakynno <fakynno@gmail.com>
2023-01-14 11:57:47 -0300

    refatoracao do metodo

--- src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
+++ src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
@@ -2,38 +2,40 @@ package com.algaworks.junit.utilidade;
 
 import org.junit.jupiter.api.Test;
 
+import static com.algaworks.junit.utilidade.SaudacaoUtil.saudar;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SaudacaoUtilTest {
 
     @Test
     public void saudarComBomDia() {
-        String saudacao = SaudacaoUtil.saudar(9);
-        assertEquals("Bom dia", saudacao, "Saudação Incorreta!");
+        int horaValida = 9;
+        String saudacao = saudar(horaValida);
+        assertEquals("Bom dia", saudacao);
     }
 
     @Test
     public void saudarComBoaTarde() {
-        String saudacao = SaudacaoUtil.saudar(13);
+        String saudacao = saudar(13);
         assertEquals("Boa tarde", saudacao, "Saudação incorreta!");
     }
 
     @Test
     void saudarComBoaNoite() {
-        String saudacao = SaudacaoUtil.saudar(20);
+        String saudacao = saudar(20);
         assertEquals("Boa noite", saudacao, "Saudação incorreta!");
     }
 
     @Test
     public void shouldThrowException() {
         IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class,
-                () -> SaudacaoUtil.saudar(-10));
+                () -> saudar(-10));
         assertEquals("Hora inválida", illegalArgumentException.getMessage());
     }
 
 
     @Test
     public void shouldNotThrowException() {
-        assertDoesNotThrow(()-> SaudacaoUtil.saudar(0));
+        assertDoesNotThrow(()-> saudar(0));
     }
 }
