d0ea27489e56ecd0dfdeadebc189df942b7b4cd3
fakynno <fakynno@gmail.com>
2023-01-22 17:25:42 -0300

    Add getters para as variaveis final

--- src/main/java/com/algaworks/junit/utilidade/ContaBancaria.java
+++ src/main/java/com/algaworks/junit/utilidade/ContaBancaria.java
@@ -1,72 +1,88 @@
-package com.algaworks.junit.utilidade;
+    package com.algaworks.junit.utilidade;
 
-import java.math.BigDecimal;
+    import java.math.BigDecimal;
 
-public class ContaBancaria {
+    public class ContaBancaria {
 
-    private BigDecimal saldo;
+        private BigDecimal saldo;
 
-    private final static String SALDO_NULO = "Saldo não pode ser nulo!";
-    private final static String SALDO_INSUFICIENTE = "Saldo insuficiente para realizar esta operação!";
-    private final static String SALDO_ZERADO = "Saldo não pode ser zerado!";
-    private final static String SALDO_NEGATIVO = "Saldo não pode ser negativo!";
+        private final static String SALDO_NULO = "Saldo não pode ser nulo!";
+        private final static String SALDO_INSUFICIENTE = "Saldo insuficiente para realizar esta operação!";
+        private final static String SALDO_ZERADO = "Saldo não pode ser zerado!";
+        private final static String SALDO_NEGATIVO = "Saldo não pode ser negativo!";
 
-    public BigDecimal getSaldo() {
-        return saldo;
-    }
-    public void setSaldo(BigDecimal saldo) {
-        this.saldo = saldo;
-    }
-
-    public ContaBancaria(BigDecimal saldo) {
-        //TODO 1 - validar saldo: não pode ser nulo, caso seja, deve lançar uma IllegalArgumentException
-        //TODO 2 - pode ser zero ou negativo
-        saldo = this.getSaldo();
-        if (saldo == null) {
-            throw new IllegalArgumentException(SALDO_NULO);
-        }
-    }
-
-    public void saque(BigDecimal valor) {
-        //TODO 1 - validar valor: não pode ser nulo, zero ou menor que zero, caso seja, deve lançar uma IllegalArgumentException
-        //TODO 2 - Deve subtrair o valor do saldo
-        //TODO 3 - Se o saldo for insuficiente deve lançar uma RuntimeException
-
-        BigDecimal resultado;
-
-        valor = valor.subtract(this.getSaldo());
-        resultado = valor;
-
-        if (valor.compareTo(BigDecimal.ZERO) < 0) {
-            throw new RuntimeException(SALDO_INSUFICIENTE);
+        public static String getSaldoNulo() {
+            return SALDO_NULO;
         }
 
-        if (resultado == null) {
-            throw new IllegalArgumentException(SALDO_NULO);
-        } else if (resultado.compareTo(BigDecimal.ZERO) < 0) {
-            throw new IllegalArgumentException(SALDO_NEGATIVO);
-        } else if (resultado.compareTo(BigDecimal.ZERO) == 0) {
-            throw new IllegalArgumentException(SALDO_ZERADO);
+        public static String getSaldoInsuficiente() {
+            return SALDO_INSUFICIENTE;
+        }
+
+        public static String getSaldoNegativo() {
+            return SALDO_NEGATIVO;
+        }
+
+        public static String getSaldoZerado() {
+            return SALDO_ZERADO;
+        }
+
+        public BigDecimal getSaldo() {
+            return saldo;
+        }
+        public void setSaldo(BigDecimal saldo) {
+            this.saldo = saldo;
+        }
+
+        public ContaBancaria(BigDecimal saldo) {
+            //TODO 1 - validar saldo: não pode ser nulo, caso seja, deve lançar uma IllegalArgumentException
+            //TODO 2 - pode ser zero ou negativo
+            saldo = this.getSaldo();
+            if (saldo == null) {
+                throw new IllegalArgumentException(SALDO_NULO);
+            }
+        }
+
+        public void saque(BigDecimal valor) {
+            //TODO 1 - validar valor: não pode ser nulo, zero ou menor que zero, caso seja, deve lançar uma IllegalArgumentException
+            //TODO 2 - Deve subtrair o valor do saldo
+            //TODO 3 - Se o saldo for insuficiente deve lançar uma RuntimeException
+
+            BigDecimal resultado;
+
+            valor = valor.subtract(this.getSaldo());
+            resultado = valor;
+
+            if (valor.compareTo(BigDecimal.ZERO) < 0) {
+                throw new RuntimeException(SALDO_INSUFICIENTE);
+            }
+
+            if (resultado == null) {
+                throw new IllegalArgumentException(SALDO_NULO);
+            } else if (resultado.compareTo(BigDecimal.ZERO) < 0) {
+                throw new IllegalArgumentException(SALDO_NEGATIVO);
+            } else if (resultado.compareTo(BigDecimal.ZERO) == 0) {
+                throw new IllegalArgumentException(SALDO_ZERADO);
+            }
+        }
+
+        public void deposito(BigDecimal valor) {
+            //TODO 1 - validar valor: não pode ser nulo, zero ou menor que zero, caso seja, deve lançar uma IllegalArgumentException
+            //TODO 2 - Deve adicionar o valor ao saldo
+
+            valor = valor.add(this.getSaldo());
+
+            if (valor == null) {
+                throw new IllegalArgumentException(SALDO_NULO);
+            } else if (valor.compareTo(BigDecimal.ZERO) < 0) {
+                throw new IllegalArgumentException(SALDO_NEGATIVO);
+            } else if (valor.compareTo(BigDecimal.ZERO) == 0) {
+                throw new IllegalArgumentException(SALDO_ZERADO);
+            }
+        }
+
+        public BigDecimal saldoConta() {
+            //TODO 1 - retornar saldo
+            return this.getSaldo();
         }
     }
-
-    public void deposito(BigDecimal valor) {
-        //TODO 1 - validar valor: não pode ser nulo, zero ou menor que zero, caso seja, deve lançar uma IllegalArgumentException
-        //TODO 2 - Deve adicionar o valor ao saldo
-
-        valor = valor.add(this.getSaldo());
-
-        if (valor == null) {
-            throw new IllegalArgumentException(SALDO_NULO);
-        } else if (valor.compareTo(BigDecimal.ZERO) < 0) {
-            throw new IllegalArgumentException(SALDO_NEGATIVO);
-        } else if (valor.compareTo(BigDecimal.ZERO) == 0) {
-            throw new IllegalArgumentException(SALDO_ZERADO);
-        }
-    }
-
-    public BigDecimal saldoConta() {
-        //TODO 1 - retornar saldo
-        return this.getSaldo();
-    }
-}
--- src/test/java/com/algaworks/junit/utilidade/ContaBancariaTest.java
+++ src/test/java/com/algaworks/junit/utilidade/ContaBancariaTest.java
@@ -1,7 +1,16 @@
 package com.algaworks.junit.utilidade;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ContaBancariaTest {
 
+    private final static String SALDO_NULO = ContaBancaria.getSaldoNulo();
+
+    @Test
+    void saldoNaoPodeSerNulo() {
+        assertThrows(IllegalArgumentException.class, () -> new ContaBancaria(null),
+                SALDO_NULO);
+    }
 }
